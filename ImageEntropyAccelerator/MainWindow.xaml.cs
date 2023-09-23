47d6173711405abb10772e4277fe28c623555f35
bforester <bogdan.forest.job@gmail.com>
2023-09-23 20:57:20 +0300

    First commit

---
+++ ImageEntropyAccelerator/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ImageEntropyAccelerator
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void StartProcessConversion(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
